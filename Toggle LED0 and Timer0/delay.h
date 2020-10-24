0bbda76f515a60154c5605baf53ec93fb7ad4e81
JESDevCom <39608694+JESDevCom@users.noreply.github.com>
2020-10-24 16:14:44 -0500

    Add files via upload

---
+++ Toggle LED0 and Timer0/delay.h
@@ -0,0 +1,16 @@
+/*
+ * delay.h
+ *
+ * Created: 10/19/2020 1:30:48 PM
+ *  Author: John
+ */ 
+
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+#include "sam.h"
+
+void delay(void);
+
+#endif /* DELAY_H_ */
