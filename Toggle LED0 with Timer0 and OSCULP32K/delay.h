b576d67d8b17db90e2f85ecfc5865eaeea43007e
JESDevCom <39608694+JESDevCom@users.noreply.github.com>
2020-10-26 19:40:10 -0500

    Add files via upload

---
+++ Toggle LED0 with Timer0 and OSCULP32K/delay.h
@@ -0,0 +1,18 @@
+/*
+ * delay.h
+ *
+ * Created: 10/24/2020 5:50:55 PM
+ *  Author: John
+ */ 
+
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+#include "sam.h"
+
+void delay(void);
+
+
+
+#endif /* DELAY_H_ */
